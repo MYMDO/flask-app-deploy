0087635cc265483ba35fb43a1d0b015c5eb42d12
Y <thejfex@proton.me>
2025-02-20 10:37:35 -0600

    Edit app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Привіт, світ! Це простий Flask застосунок, розгорнутий на AWS EC2 за допомогою Terraform та Ansible!'
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
